138fd5aecb8572b1792388a6c0c97ec1806f196d
lizamania <hifik8@gmail.com>
2022-08-17 16:41:44 +0600

    Initial commit

---
+++ src/Company/Main.java
@@ -0,0 +1,10 @@
+import Company.MagicDoor;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MagicDoor action = new MagicDoor();
+        System.out.println(action.toReturn());
+    }
+
+}
