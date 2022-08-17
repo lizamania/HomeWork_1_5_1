28dd89b57ba23174a1d47afda60441e5dc728a1b
Liza <110455437+lizamania@users.noreply.github.com>
2022-08-17 16:47:14 +0600

    Update MagicDoor.java

--- src/Company/MagicDoor.java
+++ src/Company/MagicDoor.java
@@ -1,2 +1,12 @@
-package Company;public class MagicDoor {
+package Company;
+public class MagicDoor {
+  
+    public Hero[] toReturn(){
+        Hero hero1 = new Hero(250, 20, 50);
+        Hero hero2 = new Hero(250, 20, 50);
+        Hero hero3 = new Hero(250, 20, 50);
+        Hero hero4 = new Hero(250, 20, 50);
+        Hero[] arrayOfHeroes = {hero1, hero2, hero3, hero4};
+        return arrayOfHeroes;
+    }
 }
